9c052ed355589dafcf236eedf730bde5d03589a0
Muhammad Raza <mohammadrazaarain10@gmail.com>
2025-02-20 19:34:51 +0500

    fixing

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('This is nodejs')
+const express = require("express");
+const app = express();
+
+app.use(express.json()); // Middleware for parsing JSON
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express on Vercel!");
+});
+
+module.exports = app; // Export the app for serverless function
