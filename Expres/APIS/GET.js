be0ffeba3ead8137d339afb48717f2725f964f1c
Muhammad Raza <mohammadrazaarain10@gmail.com>
2025-02-20 19:39:33 +0500

    fixing

--- Expres/APIS/GET.js
+++ Expres/APIS/GET.js
@@ -1,3 +1,24 @@
+// const express = require("express");
+// const restaurantModal = require("../Models/restaurantSchema");
+// const connectdb = require("../db/dbConnection");
+
+// const app = express();
+
+// connectdb();
+// app.use("/uploads", express.static("uploads"));
+
+// app.get("/restaurants", async (req, res) => {
+//   try {
+//     const restaurants = await restaurantModal.find();
+//     res.json(restaurants);
+//   } catch (error) {
+//     console.log("Error fetching Data", error);
+//     res.status(500).send("Server Error");
+//   }
+// });
+// app.listen(2000, () => {
+//   console.log("server is running on porrt 2000");
+// });
 const express = require("express");
 const restaurantModal = require("../Models/restaurantSchema");
 const connectdb = require("../db/dbConnection");
@@ -16,6 +37,11 @@ app.get("/restaurants", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-app.listen(2000, () => {
-  console.log("server is running on porrt 2000");
-});
+
+// ❌ Remove app.listen() ❌
+// app.listen(2000, () => {
+//   console.log("server is running on port 2000");
+// });
+
+// ✅ Export app for Vercel
+module.exports = app;
