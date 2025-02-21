1929e29a57f0fd4b13d61f857055b01bae86d5e2
Muhammad Raza <mohammadrazaarain10@gmail.com>
2025-02-21 23:12:39 +0500

    output deepseek

--- Expres/APIS/GET.js
+++ Expres/APIS/GET.js
@@ -1,13 +1,30 @@
+// const express = require("express");
+// const restaurantModal = require("../Models/restaurantSchema");
+// const connectdb = require("../db/dbConnection");
+
+// const app = express();
+
+// connectdb();
+// app.use("/uploads", express.static("uploads"));
+
+// app.get("/restaurants", async (req, res) => {
+//   try {
+//     const restaurants = await restaurantModal.find();
+//     res.json(restaurants);
+//   } catch (error) {
+//     console.log("Error fetching Data", error);
+//     res.status(500).send("Server Error");
+//   }
+// });
+// app.listen(2000, () => {
+//   console.log("server is running on porrt 2000");
+// });
+// Expres/APIS/GET.js
 const express = require("express");
 const restaurantModal = require("../Models/restaurantSchema");
-const connectdb = require("../db/dbConnection");
+const router = express.Router();
 
-const app = express();
-
-connectdb();
-app.use("/uploads", express.static("uploads"));
-
-app.get("/restaurants", async (req, res) => {
+router.get("/restaurants", async (req, res) => {
   try {
     const restaurants = await restaurantModal.find();
     res.json(restaurants);
@@ -16,6 +33,5 @@ app.get("/restaurants", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-app.listen(2000, () => {
-  console.log("server is running on porrt 2000");
-});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('This is index.js file')
+// index.js
+const express = require("express");
+const app = express();
+const connectdb = require("./Expres/db/dbConnection");
+const restaurantRoutes = require("./Expres/APIS/GET");
+
+connectdb();
+
+app.use("/uploads", express.static("uploads"));
+app.use("/api", restaurantRoutes);
+
+module.exports = app;
