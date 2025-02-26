ef40534e9e20b1823259b7afd9d2aef4ad5d1b8f
Muhammad Raza <mohammadrazaarain10@gmail.com>
2025-02-26 12:04:36 +0500

    add cors to work api on every domain

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectdb = require("./Expres/db/dbConnection");
 const restaurantRoutes = require("./Routes/restaurants");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
+
 
 const app = express();
 connectdb();
@@ -14,6 +16,8 @@ app.use(
 
 // Routes
 app.use(restaurantRoutes);
+app.use(cors()); // 🔥 Enable CORS for all domains
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Foodpanda backend");
